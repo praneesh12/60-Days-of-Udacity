aeb66bf12876d24e6fefb75e7627d76221db07cd
Praneesh Khanna <khanna.praneesh@gmail.com>
2019-06-27 23:16:15 -0400

    adding fashion-mnist

---
+++ fashion_mnist.py
@@ -0,0 +1,20 @@
+import torch
+from torchvision import datasets, transforms
+from torch import nn
+from torch import nn, optim
+import torch.nn.functional as F
+
+import torch
+from torchvision import datasets, transforms
+# import helper
+
+# Define a transform to normalize the data
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+# Download and load the training data
+trainset = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/', download=True, train=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+
+# Download and load the test data
+testset = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/', download=True, train=False, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=True)
